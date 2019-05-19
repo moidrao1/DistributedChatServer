806a968d41d1a555cf9fd6339469a490b9c372cd
Moeed <abdulfastian1@gmail.com>
2019-05-19 14:30:13 +0500

    Client Code inorder to connect to Server

---
+++ ChatClient/ChatClient/SystemProperties.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatClient
+{
+    class SystemProperties
+    {
+        
+        public static int ClientStartRange;
+        public static string logfilePath = string.Empty;
+        public static int TotalClients;
+        public static int TotalServers;
+        public static int senderID;
+        public static int SleepTime ;
+        public static string LBIP = String.Empty;
+        public static string ServerIP = String.Empty;
+        public static int LBPort = 0;
+        public static int ServerPort = 0;
+
+    }
+}
